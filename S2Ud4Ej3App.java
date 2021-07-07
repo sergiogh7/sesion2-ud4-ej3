75420cc71e13e955a8a7eff4fe36ef8dbc12bbf1
Sergio <87013586+sergiogh7@users.noreply.github.com>
2021-07-07 10:03:32 +0200

    Sesion2-Ud4-Ej3

---
+++ S2Ud4Ej3App.java
@@ -0,0 +1,41 @@
+/**
+ * Version 1.0
+ * @author Sergio Galimany Hoyo
+ * 
+ */
+public class S2Ud4Ej3App {
+
+	public static void main(String[] args) {
+		
+		// Variables
+		int X = 5, Y = 2;
+        double M = 1.4, N = 2.3;
+        
+        // Printa resultados.
+        System.out.println("Variable X = " + X);
+        System.out.println("Variable Y = " + Y);
+        System.out.println("Variable M = " + M);
+        System.out.println("Variable N = " + N);
+        System.out.println(X + " + " + Y + " = " + (X+Y));                                                        
+        System.out.println(X + " - " + Y + " = " + (X-Y));
+        System.out.println(X + " * " + Y + " = " + X*Y);
+        System.out.println(X + " / " + Y + " = " + X/Y);
+        System.out.println(X + " % " + Y + " = " + X%Y);
+        System.out.println(N + " + " + M + " = " + (N+M));
+        System.out.println(N + " - " + M + " = " + (N-M));
+        System.out.println(N + " * " + M + " = " + N*M);
+        System.out.println(N + " / " + M + " = " + N/M);
+        System.out.println(N + " % " + M + " = " + N%M);
+        System.out.println(X + " + " + N + " = " + (X+N));
+        System.out.println(Y + " / " + M + " = " + Y/M);
+        System.out.println(Y + " % " + M + " = " + Y%M);
+        System.out.println("Variable X = " + X + " el doble es " + 2*X);
+        System.out.println("Variable Y = " + Y + " el doble es " + 2*Y);
+        System.out.println("Variable M = " + M + " el doble es " + 2*M);
+        System.out.println("Variable N = " + N + " el doble es " + 2*N);
+        System.out.println(X + " + " + Y + " + " + N + " + " + M + " = " + (X+Y+M+N));
+        System.out.println(X + " * " + Y + " * " + N + " * " + M + " = " + (X*Y*M*N));
+
+	}
+
+}
